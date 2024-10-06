75ac86bf6487d681eb756da860289afa6af7101f
Awalhossain <waqin21@gmail.com>
2024-10-06 15:21:31 +0600

    Refactor UsersController to include CRUD operations for users

--- nest-mastery/src/users/users.controller.ts
+++ nest-mastery/src/users/users.controller.ts
@@ -1,11 +1,17 @@
 import {
-    Body,
-    Controller,
-    Post,
-    UsePipes,
-    ValidationPipe,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UpdateUserDto } from './dtos/updateUser.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -16,5 +22,26 @@ export class UsersController {
   @UsePipes(ValidationPipe)
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
+  }
+
+  @Get()
+  getUsers() {
+    return this.userService.getUsers();
+  }
+
+  @Get(':id')
+  getUserByID(@Param('id', ParseIntPipe) id: number ) {
+    return this.userService.getUserByID(id);
+  }
+
+  @Patch(':id')
+  @UsePipes(ValidationPipe)
+  updateUserByID(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.updateUserByID(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  deleteUserByID(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.deleteUserByID(id);
   }
 }
