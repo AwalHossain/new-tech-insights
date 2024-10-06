21f746cc9ce472a126c9a4236632e801bc475a94
Awalhossain <waqin21@gmail.com>
2024-10-06 15:21:38 +0600

    Refactor UsersService to include CRUD operations for users

--- nest-mastery/src/users/users.service.ts
+++ nest-mastery/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -12,7 +12,48 @@ export class UsersService {
     });
   }
 
-  getUsers() {}
+  getUsers() {
+    return this.prisma.user.findMany();
+  }
 
-  getUserByID() {}
+  getUserByID(id: number) {
+    return this.prisma.user.findUnique({
+      where: {id}
+    });
+  }
+
+  async updateUserByID(id: number, data: Prisma.UserUpdateInput){
+    const user = await this.prisma.user.findFirst({
+      where: {id}
+    })
+    if(!user){
+      throw new HttpException('User not found', 404)
+    }
+    if(data.username){
+      let userInfo = await this.prisma.user.findUnique({
+        where:{username: user.username as string}
+      })
+      if(userInfo){
+        throw new HttpException('Username already found', 404)
+      }
+
+      return this.prisma.user.update({
+        where: {id},
+        data
+      });
+    }
+  }
+
+
+  async deleteUserByID(id: number) {
+    const user = await this.prisma.user.findFirst({
+      where: {id}
+    })
+    if(!user){
+      throw new HttpException('User not found', 404)
+    }
+    return this.prisma.user.delete({
+      where: {id}
+    });
+  }
 }
